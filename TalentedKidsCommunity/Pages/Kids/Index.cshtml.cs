f295e36470401e4fcf0013e1a677e99e0241d6e2
muaz-aldairi <59302571+muaz-aldairi@users.noreply.github.com>
2024-02-21 11:05:32 +1300

    sorting Kids by LastName, Age, and EnrollmentDate

--- TalentedKidsCommunity/Pages/Kids/Index.cshtml.cs
+++ TalentedKidsCommunity/Pages/Kids/Index.cshtml.cs
@@ -19,11 +19,52 @@ namespace TalentedKidsCommunity.Pages.Kids
             _context = context;
         }
 
-        public IList<Kid> Kid { get;set; } = default!;
+        // sorting and filtering properties
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string AgeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+
+        public IList<Kid> Kids { get; set; } = default!;
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Kid = await _context.Kids.ToListAsync();
+
+
+            // sorting functionality by last name, age , and enrollment date
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "date-asc" ? "date_desc" : "date-asc";
+            AgeSort = sortOrder == "age-asc" ? "age_desc" : "age-asc";
+
+            IQueryable<Kid> kidsIQ = from k in _context.Kids
+                                     select k;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    kidsIQ = kidsIQ.OrderByDescending(k => k.LastName);
+                    break;
+                case "date-asc":
+                    kidsIQ = kidsIQ.OrderBy(k => k.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    kidsIQ = kidsIQ.OrderByDescending(k => k.EnrollmentDate);
+                    break;
+                case "age-asc":
+                    kidsIQ = kidsIQ.OrderBy(k => k.Age);
+                    break;
+                case "age_desc":
+                    kidsIQ = kidsIQ.OrderByDescending(k => k.Age);
+                    break;
+                default:
+                    kidsIQ = kidsIQ.OrderBy(k => k.LastName);
+                    break;
+            }
+
+
+            Kids = await kidsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
